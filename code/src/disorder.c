7221bef1b9bc3f59b097976d8830fb38042231b3
Michael E. Locasto <michael@freshdefense.net>
2010-08-27 09:08:09 -0600

    push log2 Makefile fix and FreeBSD macro definition

--- code/src/disorder.c
+++ code/src/disorder.c
@@ -25,6 +25,10 @@
 #include <stdio.h> //for NULL
 #include "../include/disorder.h"
 
+#if defined(__FreeBSD__)
+#define        log2(x) (log((x)) * (1./M_LN2))
+#endif
+
 /** Frequecies for each byte */
 static int m_token_freqs[LIBDO_MAX_BYTES]; //frequency of each token in sample
 static float m_token_probs[LIBDO_MAX_BYTES]; //P(each token appearing)
